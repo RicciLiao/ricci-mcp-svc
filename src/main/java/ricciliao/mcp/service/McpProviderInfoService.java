f742d365550cd2b24687f1d7e20b651f0881d0ea
ricciliao <riccix@163.com>
2026-04-09 10:41:45 +0800

    refactor: code review.

--- src/main/java/ricciliao/mcp/service/McpProviderInfoService.java
+++ src/main/java/ricciliao/mcp/service/McpProviderInfoService.java
@@ -2,11 +2,11 @@ package ricciliao.mcp.service;
 
 import ricciliao.mcp.pojo.bo.McpProviderInfoBo;
 import ricciliao.mcp.pojo.dto.McpProviderInfoDto;
-import ricciliao.x.component.persistence.CurdService;
+import ricciliao.x.component.persistence.CrudService;
 
 import java.util.List;
 
-public interface McpProviderInfoService extends CurdService<McpProviderInfoDto> {
+public interface McpProviderInfoService extends CrudService<McpProviderInfoDto> {
 
     McpProviderInfoBo fullyGet(Long id);
 
